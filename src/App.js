9b9c4a4de8ad4c590e42febe443ffd28c88a9b98
Raul Torres <jr.torsa@gmail.com>
2021-07-09 23:25:32 -0600

    resolviendo el ejercico

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    letters: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      letters: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" value={this.state.letters} onChange={this.handleChange}></textarea>
+        <div className="counter">{this.state.letters.length}</div>
       </div>
     );
   }
